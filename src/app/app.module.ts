d381eed53952c25a513d9b98d5a811b75618cdae
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2022-12-01 20:08:06 -0600

    Edit dashboard-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
